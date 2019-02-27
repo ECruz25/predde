774a5d2acd779a453aac6d5d179e65d3f3ca4b37
Edwin Cruz <edwincruz25@unitec.edu>
2019-02-26 21:40:16 -0600

    actualizando app

--- app.js
+++ app.js
@@ -20,7 +20,24 @@ app.use(expressValidator());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
+//Static file declaration
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+//production mode
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  //
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  })
+}
+//build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
+
+// app.use(express.static(path.join(__dirname, 'client/build')));
 app.use('/api/libros', librosRouter);
 app.use('/api/categorias', categoriasRouter);
 
