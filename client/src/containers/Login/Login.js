3f738b9b6366f84463e4b641f8aaefd17674faff
Franklin <fpena@caracolknits.com>
2019-04-05 15:48:01 -0600

    Login
    Login

--- client/src/containers/Login/Login.js
+++ client/src/containers/Login/Login.js
@@ -1,71 +1,80 @@
-import React from "react";
 import { NavLink } from 'react-router-dom';
-
+import React, { Component } from "react";
 import "./Login.css";
 
-const Login = props => {
-    const { title } = props;
-
-    const onSubmit = (e) => {
-        const username = e.target[0].value;
-        const password = e.target[1].value;
-
-        const response = 'pase';
-        if (response === 'pase') {
-            props.history.replace(`/menu`);
-        }
+class Login extends Component {
+    state={
+        username:'',
+        password:''
     };
-    return (
-        <div className="Init">
-            <div className="Header">
-                <div className="Welcome">
-                    <h1>Portal de Administración PREDDE</h1>
+
+    handleUsernameChange = function(e) {
+        this.setState({username:e.target.value});
+    }
+
+    handlePasswordChange = function(e){
+        this.setState({password:e.target.value});
+    }
+
+    handleSubmit(event) {
+     
+    }
+
+    render() {
+        return (
+            < div className="Init" >
+                <div className="Header">
+                    <div className="Welcome">
+                        <h1>Portal de Administración PREDDE</h1>
+                    </div>
                 </div>
-            </div>
 
-            <div className="Login">
-                <div className="Title">
-                    <h2>BIENVENIDOS</h2>
-                </div>
-
-                <form onSubmit={onSubmit}>
-                    <div className="Form-Content">
-                        <input
-                            id="login__username"
-                            type="text"
-                            name="username"
-                            class="form__input"
-                            placeholder="Username"
-                            required
-                        />
-
-                        <input
-                            id="login__username"
-                            type="text"
-                            name="password"
-                            class="form__input"
-                            placeholder="Password"
-                            required
-                        />
+                <div className="Login">
+                    <div className="Title">
+                        <h2>BIENVENIDOS</h2>
                     </div>
 
-                    <div
-                        style={{
-                            position: "absolute",
-                            marginTop: "15px",
-                            right: "80px",
-                            justifyContent: "left"
-                        }}
-                    >
+                    <form onSubmit={this.handleSubmit}>
+                        <div className="Form-Content">
+                            <input
+                                id="login__username"
+                                type="text"
+                                name="username"
+                                class="form__input"
+                                placeholder="Username"
+                                // required
+                                onChange={e => this.handleUsernameChange(e)}
+                            />
 
-                        <input type="submit" value="Sign In" />
-                    </div>
-                </form>
+                            <input
+                                id="login__username"
+                                type="text"
+                                name="password"
+                                class="form__input"
+                                placeholder="Password"
+                                // required
+                                onChange={e => this.handlePasswordChange(e)}
+                            />
+                        </div>
 
-                <div className="Fotter" />
-            </div>
-        </div>
-    );
-};
+                        <div
+                            style={{
+                                position: "absolute",
+                                marginTop: "15px",
+                                right: "80px",
+                                justifyContent: "left"
+                            }}
+                        >
+
+                            <input type="submit" value="Sign In" />
+                        </div>
+                    </form>
+
+                    <div className="Fotter" />
+                </div>
+            </div >
+        );
+    }
+}
 
 export default Login;
